c25b5525e91b9d7c43edf7945acf4d81f5c574e7
FrankBerlin <frankberlinausberlin@gmx.de>
2025-01-24 07:40:29 +0100

    add tailwindcss

---
+++ frank-to-do/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx,vue}", // Hier werden die Pfade zu deinen HTML- und JS-Dateien angegeben
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
