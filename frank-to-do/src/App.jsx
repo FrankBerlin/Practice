85b0022449a5be00fa406b205677165b3d6635f4
FrankBerlin <frankberlinausberlin@gmx.de>
2025-02-03 10:43:26 +0100

    add responsiveness

--- frank-to-do/src/App.jsx
+++ frank-to-do/src/App.jsx
@@ -30,29 +30,27 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col min-w-[360px] md:min-w-[800px] p-2 md:p-4 bg-white rounded-md shadow-md'>
+      <div className='flex flex-col min-w-[360px] md:min-w-[900px] p-2 md:p-4 bg-white rounded-md shadow-md'>
         <h1 className='pb-8 block text-grey-800'>Todo App</h1>
         <div className='p-2 flex flex-row gap-2 justify-center bg-blue-100'>
           <label><button className='shadow-md' onClick={toggleOpen}>{showOpen ? 'show all' : 'show open'}</button></label>
         </div>
         <div className='p-2 flex bg-blue-100'>
-          <ul id="todolist" className='w-full'>
+          <div id="todolist" className='w-full grid grid-cols-1 md:grid-cols-2 p-2 gap-4'>
             { todos && todos.map((todo) => (
-                <li key={todo.id}>
+                <div key={todo.id}>
                   <div className='flex flex-row justify-between bg-white p-2 mb-2'>
                     <span>{todo.text}</span>
                     <input className='w-[30px]' type='checkbox' checked={todo.done} value={newTodo} onChange={() => {toggleDone(todo.id)}} />
                   </div>
-                </li>
+                </div>
             ))}
-            <li className='mt-8'>
-              <input onChange={handleChange} className='p-[11px] mb-4 mr-4 shadow-inner rounded-md w-4/6' value={newTodo} type='text' placeholder='new todo'/>
-              <button onClick={()=>addTodo(newTodo)} className='shadow-md bg-green-400 w-1/6'>Add</button>
-            </li>
-          </ul>
-        
-        </div>
-
+          </div>
+          </div>
+          <div className='mt-8'>
+            <input onChange={handleChange} className='p-[11px] border-2 border-solid border-blue-100 mb-4 mr-4 shadow-inner rounded-md' value={newTodo} type='text' placeholder='new todo'/>
+            <button onClick={()=>addTodo(newTodo)} className='shadow-md bg-green-400'>Add</button>
+          </div>
       </div>
     </>
   )
