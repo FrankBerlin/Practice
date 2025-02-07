bd7be27fd38a9556e8a1cd699abc86dfb21ce2e4
FrankBerlin <frankberlinausberlin@gmx.de>
2025-02-07 13:14:07 +0100

    add link to github

--- frank-to-do/src/Techdetails.jsx
+++ frank-to-do/src/Techdetails.jsx
@@ -2,7 +2,8 @@ const Techdetails = () => {
     return (
         <div>
             <span className='text-m text-gray-600'>made with: </span>
-            <span className='text-m text-gray-500'>vite | react | tailwind</span>
+            <span className='text-m text-gray-500'>vite | react | tailwind </span>
+            <span className='text-m text-gray-600'>&gt; <a className="hover:shadow-md" href='https://github.com/FrankBerlin/Practice/tree/master/frank-to-do' target="_blank" >git</a></span>
         </div>
     );
 };
